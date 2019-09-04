66d244c1e2da044dab49ce1980272e8b606f563a
Artiium Arbyummi <artiium21@gmail.com>
2019-09-04 01:03:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
 counter = 0
+new_square = []
 while array[counter] do
-  puts array[counter] * array[counter]
+  new_square.push(array[counter] * array[counter])
   counter += 1
 end
   
